b87988a73533f7315eb9a9d9e9f2ba1908b7f4e9
unknown <rsomaskandan@D-MAA-00391891.corp.ebay.com>
2012-12-21 21:35:25 +0530

    Updated folder-structure for NuGet

--- Merchant.bat
+++ Merchant.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_Merchant_SDK.sln /build Release %1
+call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_Merchant_SDK\PayPal_Merchant_SDK.sln /build Release %1
 
 copy /Y PayPal_Merchant_SDK\bin\Release\PayPal_Merchant_SDK.dll Samples\PayPalAPISample\lib\. 
