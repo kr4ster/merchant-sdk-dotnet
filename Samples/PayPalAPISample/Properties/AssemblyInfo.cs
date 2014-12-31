5d94ae46f32f61ef012d4b8b120681a367f8495e
jziaja <jziaja@paypal.com>
2014-12-31 09:41:08 -0600

    Bump version

--- Samples/PayPalAPISample/Properties/AssemblyInfo.cs
+++ Samples/PayPalAPISample/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PayPal Merchant API Sample for .NET")]
-[assembly: AssemblyCompany("eBay Inc.")]
-[assembly: AssemblyProduct("PayPalAPISample")]
-[assembly: AssemblyCopyright("Copyright © eBay Inc., 2009-2014")]
+[assembly: AssemblyTitle("PayPal Merchant SDK Samples for .NET")]
+[assembly: AssemblyCompany("PayPal")]
+[assembly: AssemblyProduct("PayPalMerchantSDKSample")]
+[assembly: AssemblyCopyright("Copyright © 2009-2014 PayPal")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.13.117.0")]
-[assembly: AssemblyFileVersion("2.13.117.0")]
+[assembly: AssemblyVersion("2.14.117.0")]
+[assembly: AssemblyFileVersion("2.14.117.0")]
