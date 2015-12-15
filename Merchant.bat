25a8d69fe5db69dda98921ceeff9d7d7bad1e420
unknown <jziaja@paypalcorp.com>
2015-12-14 21:13:04 -0600

    Update build script

--- Merchant.bat
+++
@@ -1,4 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPalMerchantSDK\PayPalMerchantSDK.sln /build Release
-
-copy /Y PayPalMerchantSDK\bin\Release\PayPalMerchantSDK.dll Samples\PayPalAPISample\lib\. 
-copy /Y PayPalMerchantSDK\bin\Release\PayPalMerchantSDK.XML Samples\PayPalAPISample\lib\. 
